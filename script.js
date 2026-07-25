3cf424c3a61b041b2a74b0cd88321716c0ea9742
Sarah Belisario <sarahhaide89@gmail.com>
2026-07-25 17:18:09 -0300

    Finalizando o projeto adicionando a exibição do alert e a funcionalidade do botão de apagar itens.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const form = document.getElementById("item-form");
 const input = document.getElementById("add-item")
 const list = document.getElementById("list")
 
+const alertContainer = document.querySelector(".alert-container");
+const btnCloseAlert = document.querySelector(".close-alert-icon")
+
 let counterItems = document.querySelectorAll(".list .item").length + 1;
 
 form.addEventListener("submit", (evento) => {
@@ -30,4 +33,21 @@ form.addEventListener("submit", (evento) => {
 
   input.value = '';
   input.focus();
+})
+
+list.addEventListener("click", (event) => {
+  const btnDelete = event.target.closest(".btn-delete");
+
+  if (!btnDelete) return;
+
+
+  const itemLi = btnDelete.closest(".item");
+  if (itemLi) itemLi.remove();
+
+
+  alertContainer.classList.remove("hidden");
+});
+
+btnCloseAlert.addEventListener("click", () => {
+  alertContainer.classList.add("hidden");
 })
